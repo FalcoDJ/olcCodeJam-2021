b8b21c304116e7c663a5112a8d0be48dcab658a2
FalcoDJ <48579349+FalcoDJ@users.noreply.github.com>
2021-09-02 19:31:43 -0400

    Bat and full attacks!

--- src/Entities/Player/Player.gd
+++ src/Entities/Player/Player.gd
@@ -4,9 +4,7 @@ class_name Actor
 enum {
 	Idle=0,
 	Run=1,
-	Jump=2,
-	Slide=3,
-	Attack=4
+	Attack=2
 }
 
 var state = Idle
@@ -15,13 +13,13 @@ export var Acceleration = 200
 export var Max_Speed = 150
 export var Friction = 500
 
-onready var player_hurt_sfx = preload("res://src/Entities/Player/PlayerHurtSound.tscn")
-
 onready var hurt_box = $HurtBox
 onready var stats = $Stats
 
+onready var player_hurt_sfx = preload("res://src/Entities/Player/PlayerHurtSound.tscn")
 onready var sprite           : Sprite          = $Sprite
 onready var animation_player : AnimationPlayer = $AnimationPlayer
+onready var blink_animation_player : AnimationPlayer = $BlinkAnimationPlayer
 onready var animation_tree : AnimationTree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 
@@ -43,25 +41,29 @@ func get_input():
 	movement_norm = Vector2(int(Input.is_action_pressed("RIGHT")) - int(Input.is_action_pressed("LEFT")),
 								int(Input.is_action_pressed("DOWN")) - int(Input.is_action_pressed("UP")))
 	
-	if movement_norm.x != 0:
-		sprite.flip_h = movement_norm.x + 1
+#	if movement_norm.x != 0:
+#		sprite.flip_h = movement_norm.x + 1
 	
 	if movement_norm.length() > 0:
 		movement_norm = movement_norm.normalized()
+	
+	if Input.is_action_just_pressed("ATTACK"):
+		state = Attack
 	
 
 func update_animations():
 	if movement_norm != Vector2.ZERO:
 		animation_tree.set("parameters/idle/blend_position", movement_norm)
 		animation_tree.set("parameters/run/blend_position", movement_norm)
+		animation_tree.set("parameters/attack/blend_position", movement_norm)
 	
 	if state == Idle:
 		animation_state.travel("idle")
 	if state == Run:
 		animation_state.travel("run")
-
-#	if state == Attack:
-#		animation_player.play(attack_animation)
+	if state == Attack:
+		animation_state.travel("attack")
+		velocity = Vector2.ZERO
 
 
 func _process(delta: float) -> void:
@@ -98,3 +100,13 @@ func _on_HurtBox_area_entered(area: Area2D) -> void:
 	hurt_box.create_hit_effect()
 	var player_hurt_sound = player_hurt_sfx.instance()
 	get_tree().current_scene.add_child(player_hurt_sound)
+
+
+func _on_HurtBox_invincibility_started() -> void:
+	blink_animation_player.play("start")
+
+func _on_HurtBox_invincibility_ended() -> void:
+	blink_animation_player.play("stop")
+
+func AttackAnimation_finished() -> void:
+	state = Idle
--- src/Menus/OpeningScene/PlayerActor.gd
+++ src/Menus/OpeningScene/PlayerActor.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 var velocity = Vector2(250, 0)
 
-onready var Explosion = preload("res://assets/Effects/EnemyDeathEffect.tscn")
+onready var player_hurt_sound = preload("res://src/Entities/Player/PlayerHurtSound.tscn")
+
 var go_yet = false
 
 func _physics_process(delta: float) -> void:
@@ -10,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity = move_and_slide(velocity)
 
 func _on_DialogBox_finished_dialog() -> void:
-	add_child(Explosion.instance())
+	add_child(player_hurt_sound.instance())
 	go_yet = true
 
 
